55100212dd6209b871b3ac5cda4bc2a4a7d54292
Ashley Sommer <ashleysommer@gmail.com>
2020-01-20 09:47:50 +1000

    commit a JS-style action test

---
+++ .github/actions/action_dynamic_deploy_js/main.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+(function(){
+    console.log(arguments);
+    console.log("working in: "+__dirname);
+
+    const eventFilePath = __dirname + "/../test.txt";
+    fs.readFile(eventFilePath, function(err, fileData) {
+        if (err) {
+            throw err;
+        }
+
+        let config = {
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        };
+
+        axios.post(fileData, config).then((res) => {
+            console.log("Response: ", res);
+        }).catch((err) => {
+            console.log("error: ", err);
+        })
+    });
+})();
+
+
+
