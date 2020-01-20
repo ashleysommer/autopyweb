b56d2ff9b5128ee3824ec502f3c6816071e17f59
Ashley Sommer <ashleysommer@gmail.com>
2020-01-20 11:04:46 +1000

    use github and core packages to get required variables in js code

--- .github/actions/action_dynamic_deploy_js/index.js
+++ .github/actions/action_dynamic_deploy_js/index.js
@@ -1,36 +1,52 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
 const axios = require('axios');
 
-async function main() {
-    console.log(arguments);
-    console.log();
+async function main(argv) {
+    let deploy_endpoint;
+    let payload;
+    try {
+      deploy_endpoint = core.getInput('dynamic_deploy_endpoint');
+      // Get the JSON webhook payload for the event that triggered the workflow
+      payload = JSON.stringify(github.context.payload, undefined, 2)
+    } catch (error) {
+      core.setFailed(error.message);
+      return;
+    }
     console.log("working in: " + __dirname);
+    console.log("event payload: \n"+payload);
 
-    const event = JSON.parse(fs.readFileSync('/github/workflow/event.json', 'utf8'));
     let config = {
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         }
     };
-
-    axios.post("http://example.org", event, config).then((res) => {
-        console.log("Response: ", res);
-    }).catch((err) => {
-        console.log("error: ", err);
-    });
+    try {
+      let config = {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        }
+      };
+      let resp = axios.post(deploy_endpoint, payload, config);
+      let { data } = resp.data;
+      console.log("response: "+data)
+    } catch(error) {
+      console.log("error: "+error);
+      throw error;
+    }
     return "OK";
 }
 if (require.main === module) {
   main(process.argv)
     .then(res => {
       console.log({ res });
-      process.exitCode = 0
-    })
-    .catch(err => {
+      process.exitCode = 0;
+      core.setOutput('success', "true");
+    }).catch(err => {
       console.log({ err });
-      process.exitCode = 1
+      core.setOutput('success', "false");
+      core.setFailed(err);
+      process.exitCode = 1;
     })
 }
 
